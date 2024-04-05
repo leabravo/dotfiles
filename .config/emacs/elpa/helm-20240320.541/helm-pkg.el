f479f4f56e304d440896efc1c73632522288e6be
Leandro <leabravo@gmail.com>
2024-04-05 10:07:52 -0300

    update to use stow instead

---
+++ .config/emacs/elpa/helm-20240320.541/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20240320.541" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.7")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "45224ceb82b96267d12085f0cb116124b041e8bd" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
